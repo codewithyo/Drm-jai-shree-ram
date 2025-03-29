51ea1e7a2ffd917fead69e48079da80bbd930e8c
codewithyo <102235878+codewithyo@users.noreply.github.com>
2025-03-29 08:19:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
-API_ID    = os.environ.get("API_ID", "")
-API_HASH  = os.environ.get("API_HASH", "")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
-OWNER = int(os.environ.get("OWNER", "5680454765"))
+API_ID    = os.environ.get("API_ID", "28583543")
+API_HASH  = os.environ.get("API_HASH", "24fd6e53b6034ccbda0bfd62dfd47ad3")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8169345673:AAHj8ThzqWNdsSDBmyE0sFlWqOThvPn310E") 
+OWNER = int(os.environ.get("OWNER", "7612404009"))
 
-LOG = True  # Set to False if logging is not required
+LOG = False  # Set to False if logging is not required
 
 
